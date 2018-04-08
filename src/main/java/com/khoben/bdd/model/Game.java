7e91017a6fa48b934592d7b5f8c19f5284578588
khoben <extless@gmail.com>
2018-04-08 16:47:20 +0700

    added: avletters impl

--- src/main/java/com/khoben/bdd/model/Game.java
+++ src/main/java/com/khoben/bdd/model/Game.java
@@ -1,4 +1,23 @@
 package com.khoben.bdd.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private List<String> avLetters;
+
+    public Game(){
+        avLetters = new ArrayList<>();
+    }
+    public List<String> getAvLetters() {
+        return avLetters;
+    }
+
+    public void setAvLetters(List<String> avLetters) {
+        this.avLetters = avLetters;
+    }
+
+    public void deleteFromAvLetters(String letter){
+        avLetters.remove(letter);
+    }
 }
