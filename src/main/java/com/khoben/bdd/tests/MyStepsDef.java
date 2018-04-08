48d9c0bc30321d150512c9648c178320c59d05d0
khoben <extless@gmail.com>
2018-04-08 15:22:48 +0700

    fix spec

--- src/main/java/com/khoben/bdd/tests/MyStepsDef.java
+++ src/main/java/com/khoben/bdd/tests/MyStepsDef.java
@@ -8,6 +8,34 @@ import cucumber.api.java.ru.Тогда;
 import java.util.List;
 
 public class MyStepsDef {
+    @Дано("^При игре в Поле чудес даются на выбор буквы$")
+    public void при_игре_в_Поле_чудес_даются_на_выбор_буквы(List<String> arg1) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc).
+        // Field names for YourType must match the column names in
+        // your feature file (except for spaces and capitalization).
+        throw new PendingException();
+    }
+
+    @Когда("^пользователь выбрал букву \"([^\"]*)\"$")
+    public void пользователь_выбрал_букву(String arg1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Тогда("^на выбор остается$")
+    public void на_выбор_остается(List<String> arg1) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc).
+        // Field names for YourType must match the column names in
+        // your feature file (except for spaces and capitalization).
+        throw new PendingException();
+    }
+
     @Дано("^на выбор буквы$")
     public void на_выбор_буквы(List<String> arg1) {
         // Write code here that turns the phrase above into concrete actions
@@ -16,29 +44,6 @@ public class MyStepsDef {
         // E,K,V must be a scalar (String, Integer, Date, enum etc).
         // Field names for YourType must match the column names in
         // your feature file (except for spaces and capitalization).
-        throw new PendingException();
-    }
-
-    @Когда("^пользователь выбрал букву А$")
-    public void пользователь_выбрал_букву_А() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @Тогда("^на выбор остается$")
-    public void на_выбор_остается(List<String> arg1)  {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc).
-        // Field names for YourType must match the column names in
-        // your feature file (except for spaces and capitalization).
-        throw new PendingException();
-    }
-
-    @Когда("^пользователь выбрал букву A$")
-    public void пользователь_выбрал_букву_A() {
-        // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
