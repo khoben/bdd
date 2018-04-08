d7eddfa87515bb76c85f52c96a79e90f68573eb9
khoben <extless@gmail.com>
2018-04-08 15:59:56 +0700

    bdd: store selected letter and compare

--- src/main/java/com/khoben/bdd/tests/MyStepsDef.java
+++ src/main/java/com/khoben/bdd/tests/MyStepsDef.java
@@ -5,12 +5,17 @@ import cucumber.api.PendingException;
 import cucumber.api.java.ru.Дано;
 import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.Тогда;
+import org.junit.Assert;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class MyStepsDef {
     private Game game;
     private List<String> avLetters;
+    private String selectedLetter;
     @Дано("^При игре в Поле чудес даются на выбор буквы$")
     public void при_игре_в_Поле_чудес_даются_на_выбор_буквы(List<String> arg1) {
         // Write code here that turns the phrase above into concrete actions
@@ -27,7 +32,8 @@ public class MyStepsDef {
     @Когда("^пользователь выбрал букву \"([^\"]*)\"$")
     public void пользователь_выбрал_букву(String arg1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+//        throw new PendingException();
+        this.selectedLetter = arg1;
     }
 
     @Тогда("^на выбор остается$")
@@ -38,7 +44,9 @@ public class MyStepsDef {
         // E,K,V must be a scalar (String, Integer, Date, enum etc).
         // Field names for YourType must match the column names in
         // your feature file (except for spaces and capitalization).
-        throw new PendingException();
+//        throw new PendingException();
+        avLetters.remove(selectedLetter);
+        assertTrue(CollectionUtils.isEqualCollection(avLetters, arg1));
     }
 
     @Дано("^на выбор буквы$")
