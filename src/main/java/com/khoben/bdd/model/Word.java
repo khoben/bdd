f1b541f7ba46b4e0ba4f3e8728b182191badbe5e
khoben <extless@gmail.com>
2018-04-09 10:04:05 +0700

    added overrided method equals for tests purposes

--- src/main/java/com/khoben/bdd/model/Word.java
+++ src/main/java/com/khoben/bdd/model/Word.java
@@ -2,11 +2,24 @@ package com.khoben.bdd.model;
 
 public class Word {
 
-    public Word(String word, String descr) {
+    private String word;
+    private String descr;
 
+    public Word(String word, String descr) {
+        this.descr = descr;
+        this.word = word;
     }
 
     public Word() {
 
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj==this) return true;
+        if (obj==null || obj.getClass()!=this.getClass()) return false;
+        if (((Word)obj).word.equals(this.word) && ((Word)obj).descr.equals(this.descr)) return true;
+        else
+            return false;
     }
 }
